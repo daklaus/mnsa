3d8234c0c29f068d862ff79c3d312394addcd85b
Christian Ohrfandl <christian.ohrfandl@drei.at>
2013-12-14 02:20:17 +0100

    Working Nfc Tests

--- proxymidlet/src/main/java/at/ac/tuwien/mnsa/ue1/sc/TestSmartCard.java
+++ proxymidlet/src/main/java/at/ac/tuwien/mnsa/ue1/sc/TestSmartCard.java
@@ -14,7 +14,7 @@ import at.ac.tuwien.mnsa.ue1.proxymidlet.ProxyMIDlet;
 // Communication API
 //
 public class TestSmartCard implements TargetListener, TransactionListener {
-	
+
 	private OutputStream os = null;
 
 	public TestSmartCard(OutputStream os) {
@@ -32,11 +32,11 @@ public class TestSmartCard implements TargetListener, TransactionListener {
 		String targetUID = "";
 		TargetProperties target = properties[0];
 		Class[] classes = target.getConnectionNames();
-		
+
 		for (int i = 0; i < classes.length; i++) {
 			try {
 				targetUID = target.getUid();
-				ProxyMIDlet.textbox.setString(targetUID);
+				ProxyMIDlet.textbox.setString(ProxyMIDlet.textbox.getString() + targetUID + "\n");
 				os.write(targetUID.getBytes("UTF-8"));
 				os.flush();
 				if (classes[i]
@@ -48,15 +48,24 @@ public class TestSmartCard implements TargetListener, TransactionListener {
 							.open(url);
 					// Generate command
 					// Send command to smart card
-					// byte[] response = smc.exchangeData(commands);
+					byte[] response = smc
+							.exchangeData(new byte[] { (byte) 0x00,
+									(byte) 0xA4, (byte) 0x04, (byte) 0x00 });
 					// handle response
+					ProxyMIDlet.textbox.setString(ProxyMIDlet.textbox.getString() + bytesToHex(response) + "\n");
+					os.write(" --- ".getBytes("UTF-8"));
+					os.flush();
+					os.write(bytesToHex(response).getBytes("UTF-8"));
+					os.flush();
+					os.write("\n".getBytes("UTF-8"));
+					os.flush();
 				}
 			} catch (ClassNotFoundException e) {
 				// handle exception
 			} catch (IOException e) {
 				// handle exception
-				// } catch (ContactlessException ce) {
-				// // handle exception
+			} catch (ContactlessException ce) {
+				// handle exception
 			}
 		}
 	}
@@ -64,5 +73,20 @@ public class TestSmartCard implements TargetListener, TransactionListener {
 	// External reader has been detected (tag emulation mode)
 	public void externalReaderDetected(byte slot) {
 		// update the UI based on the application ID received
+	}
+	
+	
+	// From http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	
+	private static String bytesToHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for (int j = 0; j < bytes.length; j++) {
+			v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
 	}
 }
