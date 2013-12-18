7f3e649e0c6217d251a7afdae05c30c73bc915e5
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2013-12-18 23:50:01 +0100

    Finished UE2; tests cases are still needed

--- ue2/jcardcalc/src/main/java/at/ac/tuwien/mnsa/ue2/jcardcalc/JCardCalc.java
+++ ue2/jcardcalc/src/main/java/at/ac/tuwien/mnsa/ue2/jcardcalc/JCardCalc.java
@@ -1,5 +1,114 @@
 package at.ac.tuwien.mnsa.ue2.jcardcalc;
 
-public class JCardCalc {
+import javacard.framework.APDU;
+import javacard.framework.ISO7816;
+import javacard.framework.Applet;
+import javacard.framework.ISOException;
+import javacard.framework.Util;
+
+public class JCardCalc extends Applet {
+
+	// Constants for Instructions
+	public static final byte NOP = (byte) 0x00;
+	public static final byte ADD = (byte) 0x01;
+	public static final byte SUB = (byte) 0x02;
+	public static final byte MUL = (byte) 0x03;
+	public static final byte DIV = (byte) 0x04;
+	public static final byte AND = (byte) 0x05;
+	public static final byte OR = (byte) 0x06;
+	public static final byte NOT = (byte) 0x07;
+
+	/**
+	 * Calls {@link #register()}.
+	 */
+	protected JCardCalc() {
+		register();
+	}
+
+	/**
+	 * Calls the constructor {@link #JCardCalc()}.
+	 * 
+	 * @see Applet#install(byte[], short, byte)
+	 */
+	public static void install(byte[] bArray, short bOffset, byte bLength) {
+		new JCardCalc();
+	}
+
+	/**
+	 * Processes an incoming APDU.
+	 * 
+	 * @see {@link APDU}, {@link Applet#process(APDU)}
+	 * @param apdu
+	 *            the incoming APDU
+	 */
+	@Override
+	public void process(APDU apdu) {
+		// Good practice: Return 9000 on SELECT
+		if (selectingApplet()) {
+			return;
+		}
+
+		byte[] buf = apdu.getBuffer();
+		short p1 = buf[ISO7816.OFFSET_P1];
+		short p2 = buf[ISO7816.OFFSET_P2];
+		short result = 0;
+
+		switch (buf[ISO7816.OFFSET_INS]) {
+		case NOP:
+			result = 0;
+			break;
+		case ADD:
+			result = (short) (p1 + p2);
+			break;
+		case SUB:
+			result = (short) (p1 - p2);
+			break;
+		case MUL:
+			result = (short) (p1 * p2);
+			break;
+		// case DIV:
+		// break;
+		case AND:
+			result = (short) (p1 & p2);
+			break;
+		case OR:
+			result = (short) (p1 | p2);
+			break;
+		case NOT:
+			result = (short) (~p1);
+			break;
+		default:
+			// Good practice: If you don't know the INStruction, say so
+			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+		}
+
+		// Convert short to byte[2], then copy to the output buffer
+		Util.arrayCopyNonAtomic(toBytes(result), (short) 0, buf, (short) 0,
+				(short) 2);
+		// Set the length of the output buffer and flush
+		apdu.setOutgoingAndSend((short) 0, (short) 2);
+	}
+
+	/**
+	 * Helper function to convert a short to byte array.
+	 * 
+	 * @param s
+	 *            short which needs to be converted to a byte array
+	 * @return byte array with length of 2 containing the high and low byte of
+	 *         the short.
+	 */
+	public static byte[] toBytes(short s) {
+		// byte highx = (byte) ((s & 0xFF00) >> 8);
+		// Causes a memory leak by allocating memory on the heap EVERY time it
+		// is called. You should shift right then mask
+
+		// 1. Shift by 8
+		// 2. Cast to byte
+		// 3. Mask as Byte
+		// 4. Cast to byte
+		byte high = (byte) ((byte) (s >> 8) & 0xff);
+		byte low = (byte) (s & 0xff);
+		return new byte[] { high, low };
+	}
 
 }
---
+++ ue2/jcardcalc/src/test/java/at/ac/tuwien/mnsa/ue2/jcardcalc/JCardCalcTest.java
@@ -0,0 +1,150 @@
+package at.ac.tuwien.mnsa.ue2.jcardcalc;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import javacard.framework.AID;
+import javacard.framework.Applet;
+
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.licel.jcardsim.base.Simulator;
+
+@SuppressWarnings("restriction")
+public class JCardCalcTest {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(JCardCalcTest.class);
+
+	// Constants
+	private static final AID AID = new AID(hexToBytes("8985C0E78B8117F6D8"),
+			(short) 0, (byte) 9);
+	private static final Class<? extends Applet> CLAZZ = JCardCalc.class;
+
+	private static final byte[] SW_OK = new byte[] { (byte) 0x90, (byte) 0x00 };
+	private static final byte[] ZERO_RESPONSE = new byte[] { 0x00, 0x00 };
+	private static final byte ZERO = 0x00;
+
+	// Fields
+	private Simulator simulator;
+
+	@Before
+	public void setUp() throws Exception {
+		simulator = new Simulator();
+		simulator.installApplet(AID, CLAZZ);
+		simulator.selectApplet(AID);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		simulator.reset();
+		simulator.resetRuntime();
+	}
+
+	@Test
+	public void testNop() {
+		assertCalcResponse(JCardCalc.NOP, ZERO, ZERO, ZERO_RESPONSE, SW_OK);
+	}
+
+	@Test
+	public void testAdd() {
+		// TODO Write tests like testNop; mind 10 per test method; cover all
+		// cases; maybe log something with static logger LOG
+	}
+
+	@Test
+	public void testSub() {
+		// TODO Write tests like testNop; mind 10 per test method; cover all
+		// cases; maybe log something with static logger LOG
+	}
+
+	@Test
+	public void testMul() {
+		// TODO Write tests like testNop; mind 10 per test method; cover all
+		// cases; maybe log something with static logger LOG
+	}
+
+	@Test
+	public void testAnd() {
+		// TODO Write tests like testNop; mind 10 per test method; cover all
+		// cases; maybe log something with static logger LOG
+	}
+
+	@Test
+	public void testOr() {
+		// TODO Write tests like testNop; mind 10 per test method; cover all
+		// cases; maybe log something with static logger LOG
+	}
+
+	@Test
+	public void testNot() {
+		// TODO Write tests like testNop; mind 10 per test method; cover all
+		// cases; maybe log something with static logger LOG
+	}
+
+	private void assertCalcResponse(byte ins, byte p1, byte p2,
+			byte[] expectedResponseData, byte[] expectedSW) {
+		assertAPDUResponse(new byte[] { 0x00, ins, p1, p2, 0x00 },
+				expectedResponseData, expectedSW);
+	}
+
+	private void assertAPDUResponse(byte[] request,
+			byte[] expectedResponseData, byte[] expectedResponseSW) {
+		// The converts to CommandAPDU and after transmission to ResponseAPDU
+		// are just for checking the right alignment of the APDUs. We would also
+		// be able to just feed the method with raw byte arrays and evaluate the
+		// raw output byte array
+		byte[] bResponse = simulator.transmitCommand(new CommandAPDU(request)
+				.getBytes());
+		ResponseAPDU response = new ResponseAPDU(bResponse);
+
+		assertArrayEquals(expectedResponseData, response.getData());
+		assertEquals(expectedResponseSW[0], (byte) response.getSW1());
+		assertEquals(expectedResponseSW[1], (byte) response.getSW2());
+	}
+
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+	/**
+	 * Original method from <a href=
+	 * "http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java"
+	 * >stackoverflow.com</a>
+	 */
+	private static final String bytesToHex(byte... bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for (int j = 0; j < bytes.length; j++) {
+			v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
+
+	/**
+	 * Original method from <a href=
+	 * "http://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java"
+	 * >stackoverflow.com</a>
+	 */
+	private static final byte[] hexToBytes(String s) {
+		// There is no checking in this method. It should only be used if you
+		// know what you are doing.
+		// TODO Check if length > 1
+		// TODO Check if lenght % 2 == 0
+		// TODO ...
+
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
+					.digit(s.charAt(i + 1), 16));
+		}
+		return data;
+	}
+
+}
