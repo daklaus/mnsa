880c192de86d2202e210dabd4d7d3f388b3ce4f1
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2014-01-11 16:50:23 +0100

    Added NumberConverter tests; corrected NumberConverter

--- ue3/common/src/main/java/at/ac/tuwien/common/binary/NumberConverter.java
+++ ue3/common/src/main/java/at/ac/tuwien/common/binary/NumberConverter.java
@@ -2,28 +2,59 @@ package at.ac.tuwien.common.binary;
 
 public class NumberConverter {
 
-	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	private final static char[] hexChars = "0123456789ABCDEF".toCharArray();
 
 	/**
-	 * Original method from <a href=
+	 * <p>
+	 * Converts an array of bytes into it's uppercase hexadecimal string
+	 * representation (e.g. "0F34ED")
+	 * </p>
+	 * <p>
+	 * <strong>Hint:</strong> Original method from <a href=
 	 * "http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java"
 	 * >stackoverflow.com</a>
+	 * </p>
+	 * 
+	 * @param bytes
+	 *            the array of input bytes
+	 * @return a String with the uppercase hexadecimal representation
 	 */
 	public static final String bytesToHex(byte... bytes) {
-		char[] hexChars = new char[bytes.length * 2];
+		if(bytes == null)
+			throw new IllegalArgumentException("Bytes array is null");
+		
+		char[] chars = new char[bytes.length * 2];
 		int v;
-		for (int j = 0; j < bytes.length; j++) {
-			v = bytes[j] & 0xFF;
-			hexChars[j * 2] = hexArray[v >>> 4];
-			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		for (int i = 0; i < bytes.length; i++) {
+			v = bytes[i] & 0xFF;
+			chars[i * 2] = hexChars[v >>> 4];
+			chars[i * 2 + 1] = hexChars[v & 0x0F];
 		}
-		return new String(hexChars);
+		return new String(chars);
 	}
 
 	/**
-	 * Original method from <a href=
+	 * <p>
+	 * Converts a string representation of an hexadecimal value into an array of
+	 * bytes. It ignores whitespaces (everything a \s in an regex matches),
+	 * underscores, the prefix "0x" and the postfix "h". If number represented
+	 * in the string has an odd length the number will be preceded by a zero,
+	 * i.e. the string "3F2" will get [0x03, 0xF2].
+	 * </p>
+	 * <p>
+	 * <strong>Hint:</strong> Original method from <a href=
 	 * "http://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java"
 	 * >stackoverflow.com</a>
+	 * </p>
+	 * 
+	 * @param s
+	 *            the string containing the representation of the hexadecimal
+	 *            number
+	 * @return a byte array containing the bytes of the converted number
+	 * @throws NumberFormatException
+	 *             If the string contains any character which is neither a valid
+	 *             hexadecimal character (upper or lowercase) nor one of the
+	 *             ignored characters mentioned above.
 	 */
 	public static final byte[] hexToBytes(String s) {
 		if (s == null)
--- ue3/common/src/test/java/at/ac/tuwien/common/binary/TestNumberConverter.java
+++ ue3/common/src/test/java/at/ac/tuwien/common/binary/TestNumberConverter.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.common.binary;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -33,18 +34,66 @@ public class TestNumberConverter {
 	public void tearDown() throws Exception {
 	}
 
+	/* **********************************
+	 * bytesToHex *********************************
+	 */
+
 	@Test
-	public void testBytesToHex1() {
-		fail("Not yet implemented");
+	public void testBytesToHexShouldPass() {
+		assertEquals("Empty array doesn't work", "",
+				NumberConverter.bytesToHex(new byte[] {}));
+		assertEquals("Single byte doesn't work", "42",
+				NumberConverter.bytesToHex((byte) 0x42));
+		assertEquals("Multiple bytes as paramters don't work", "4F00F2",
+				NumberConverter.bytesToHex((byte) 0x4F, (byte) 0x00,
+						(byte) 0xF2));
+		assertEquals(
+				"Byte array doesn't work",
+				"4F00F2",
+				NumberConverter.bytesToHex(new byte[] { (byte) 0x4F,
+						(byte) 0x00, (byte) 0xF2 }));
 	}
 
-	/*******************************
-	 * hexToBytes
-	 *******************************/
+	@Test
+	public void testBytesToHexShouldFailNull() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("null");
+		NumberConverter.bytesToHex(null);
+	}
+
+	/* **********************************
+	 * hexToBytes *********************************
+	 */
 
 	@Test
 	public void testHexToBytesShouldPass() {
-		NumberConverter.hexToBytes("0x00");
+		assertArrayEquals("0x prefix doesn't work", new byte[] { (byte) 0x42 },
+				NumberConverter.hexToBytes("0x42"));
+		assertArrayEquals("h postfix doesn't work", new byte[] { (byte) 0x42 },
+				NumberConverter.hexToBytes("42h"));
+		assertArrayEquals("Single byte doesn't work",
+				new byte[] { (byte) 0x42 }, NumberConverter.hexToBytes("42"));
+		assertArrayEquals("Single character doesn't work",
+				new byte[] { (byte) 0x02 }, NumberConverter.hexToBytes("2"));
+		assertArrayEquals("Odd string length doesn't work",
+				NumberConverter.hexToBytes("042"), new byte[] { (byte) 0x00,
+						(byte) 0x42 });
+		assertArrayEquals("Lowercase hex-letters don't work",
+				new byte[] { (byte) 0x3f }, NumberConverter.hexToBytes("3f"));
+		assertArrayEquals("Uppercase hex-letters don't work",
+				new byte[] { (byte) 0x3f }, NumberConverter.hexToBytes("0x3F"));
+		assertArrayEquals("Spaces between don't work",
+				new byte[] { (byte) 0x5a },
+				NumberConverter.hexToBytes("0x   5a    h"));
+		assertArrayEquals("Tabs and newlines inbetween don't work", new byte[] {
+				(byte) 0x5a, (byte) 0xe6, (byte) 0x78 },
+				NumberConverter.hexToBytes(" 5 a e    6 \t  7  \n  8   h  "));
+		assertArrayEquals(
+				"Underscores and spaces as byte seperators don't work",
+				new byte[] { (byte) 0x46, (byte) 0x5f, (byte) 0x13,
+						(byte) 0x28, (byte) 0xa4, (byte) 0x63, (byte) 0x18,
+						(byte) 0xe9, (byte) 0x6b, (byte) 0x13 },
+				NumberConverter.hexToBytes("46_5F_13_28 a4_63 18_e9_6B 13"));
 	}
 
 	@Test
